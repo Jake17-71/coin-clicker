97a71a75e0d33115fdbc0554b70a5dc1c0a078d2
Dan Arhipov <chifmatsuno17@gmail.com>
2025-09-27 15:57:18 +0300

    Create Tabs & JavaScript for it

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import ThemeChangeCollection from './modules/ThemeChangeCollection.js'
 import ParticleCollection from './modules/ParticalCollection.js'
+import TabsCollection from './modules/TabsCollection.js'
 
 new ThemeChangeCollection()
 new ParticleCollection()
+new TabsCollection()
